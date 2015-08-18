d5315c663963f148bf9ab0e62a79fb256e287f34
Winnie Wu <weiwu@college.harvard.edu>
2015-08-17 22:40:17 -0700

    consolidate sensoractivity

--- app/src/main/java/com/www/lightmeter/HomeActivity.java
+++ app/src/main/java/com/www/lightmeter/HomeActivity.java
@@ -1,19 +1,27 @@
 package com.www.lightmeter;
 
-import android.annotation.TargetApi;
 import android.app.Activity;
-import android.os.Build;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.MotionEvent;
 import android.view.View;
 
 
-public class HomeActivity extends Activity {
+public class HomeActivity extends Activity implements SensorEventListener {
+    private final SensorManager mSensorManager;
+    private final Sensor lightSensor;
+
     private View isoView;
     private View apertureView;
     private View speedView;
     private View variableView;
+
+    public HomeActivity() {
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        lightSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,5 +38,21 @@ public class HomeActivity extends Activity {
 
     private void initializeListeners() {
 
+    }
+
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, lightSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    public void onSensorChanged(SensorEvent event) {
     }
 }
